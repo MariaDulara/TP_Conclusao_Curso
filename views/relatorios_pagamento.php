f87bcc1333648e6d875ad2828aa089c4a84101ea
Guilherme Santos <138945147+GuilhermeSantosRibeiro@users.noreply.github.com>
2024-07-09 20:10:54 -0300

    Update relatorios_pagamento.php

--- views/relatorios_pagamento.php
+++ views/relatorios_pagamento.php
@@ -2,14 +2,18 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style2.css"> <!-- Arquivo CSS externo -->
-    <title>Relatórios de Pagamento</title>
+    <link rel="stylesheet" href="estilo/style2.css">
+    <title>Folha de Pagamento</title>
     <link rel="shortcut icon" href="imagens/logo-proz.ico" type="image/x-icon">
 </head>
 <body>
     <header>
         <h2>Relatórios de Pagamento</h2>
+        <a href="dashboard - Copia.php" class="home-link">
+            <img src="imagens/proz.png" alt="Home">
+        </a>
     </header>
 
     <main>
